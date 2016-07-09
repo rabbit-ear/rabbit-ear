cc1f6c60fdb112afde9f1885085cfed8f8513d1f
robbykraft <robbykraft@gmail.com>
2016-07-08 22:14:13 -0400

    better line crossing algorithm

--- graph.cpp
+++ graph.cpp
@@ -12,95 +12,15 @@
 #include <stdio.h>
 #include <math.h>
 
-
-// Given three colinear points a, b, r, the function checks if
-// point b lies on line segment 'ar'
-bool onSegment(Vertex a, Vertex b, Vertex r){
-    if (b.x <= max(a.x, r.x) && b.x >= min(a.x, r.x) &&
-        b.y <= max(a.y, r.y) && b.y >= min(a.y, r.y))
-        return true;
-    return false;
-}
-
-// To find orientation of ordered triplet (a, b, r).
-// The function returns following values
-// 0 --> a, b and r are colinear
-// 1 --> Clockwise
-// 2 --> Counterclockwise
-int orientation(Vertex a, Vertex b, Vertex r){
-    int val = (b.y - a.y) * (r.x - b.x) - (b.x - a.x) * (r.y - b.y);
-    if (val == 0) return 0;  // colinear
-    return (val > 0)? 1: 2; // clock or counterclock wise
-}
-
-// The main function that returns true if line segment 'a1a2'
-// and 'b1b2' intersect.
-//bool doIntersect(Vertex a1, Vertex a2, Vertex b1, Vertex b2){
-//    // Find the four orientations needed for general and
-//    // special cases
-//    int o1 = orientation(a1, a2, b1);
-//    int o2 = orientation(a1, a2, b2);
-//    int o3 = orientation(b1, b2, a1);
-//    int o4 = orientation(b1, b2, a2);
-//    
-//    // General case
-//    if (o1 != o2 && o3 != o4)
-//        return true;
-//    
-//    // Special Cases
-//    // a1, a2 and b1 are colinear and b1 lies on segment a1a2
-//    if (o1 == 0 && onSegment(a1, b1, a2)) return true;
-//    
-//    // a1, a2 and b1 are colinear and b2 lies on segment a1a2
-//    if (o2 == 0 && onSegment(a1, b2, a2)) return true;
-//    
-//    // b1, b2 and a1 are colinear and a1 lies on segment b1b2
-//    if (o3 == 0 && onSegment(b1, a1, b2)) return true;
-//    
-//    // b1, b2 and a2 are colinear and a2 lies on segment b1b2
-//    if (o4 == 0 && onSegment(b1, a2, b2)) return true;
-//    
-//    return false; // Doesn't fall in any of the above cases
-//}
-
-bool doIntersect(Vertex a1, Vertex a2, Vertex a3, Vertex a4) {
-    float x = ((a1.x*a2.y-a1.y*a2.x)*(a3.x-a4.x)-(a1.x-a2.x)*(a3.x*a4.y-a3.y*a4.x))/((a1.x-a2.x)*(a3.y-a4.y)-(a1.y-a2.y)*(a3.x-a4.x));
-    float y = ((a1.x*a2.y-a1.y*a2.x)*(a3.y-a4.y)-(a1.y-a2.y)*(a3.x*a4.y-a3.y*a4.x))/((a1.x-a2.x)*(a3.y-a4.y)-(a1.y-a2.y)*(a3.x-a4.x));
-    if (0){//isNaN(x)||isNaN(y)) {
-        return false;
-    } else {
-        if (a1.x>=a2.x) {
-            if (!(a2.x<=x&&x<=a1.x)) {return false;}
-        } else {
-            if (!(a1.x<=x&&x<=a2.x)) {return false;}
-        }
-        if (a1.y>=a2.y) {
-            if (!(a2.y<=y&&y<=a1.y)) {return false;}
-        } else {
-            if (!(a1.y<=y&&y<=a2.y)) {return false;}
-        }
-        if (a3.x>=a4.x) {
-            if (!(a4.x<=x&&x<=a3.x)) {return false;}
-        } else {
-            if (!(a3.x<=x&&x<=a4.x)) {return false;}
-        }
-        if (a3.y>=a4.y) {
-            if (!(a4.y<=y&&y<=a3.y)) {return false;}
-        } else {
-            if (!(a3.y<=y&&y<=a4.y)) {return false;}
-        }
-    }
-    return true;
-}
+//////////////////////////////////////////////////////////////////
+//#include "algorithms2d.h"
+bool onSegment(Vertex a, Vertex point, Vertex b);
+int orientation(Vertex p, Vertex q, Vertex r);
+bool doIntersect(Vertex p1, Vertex q1, Vertex p2, Vertex q2);
+//////////////////////////////////////////////////////////////////
 
 
-/////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
-
-
-
+static float EPSILON = 0.001;
 
 Graph::Graph(int initial){
     switch (initial) {
@@ -113,13 +33,44 @@ Graph::Graph(int initial){
     }
 }
 
+void Graph::clear(){
+    vertices.clear();
+    edges.clear();
+}
+
+unsigned int Graph::numEdges(){
+    return edges.size();
+}
+
+unsigned int Graph::numVertices(){
+    return vertices.size();
+}
+
+
+// quick and easy, use a square bounding box
+bool Graph::hVerticesEqual(Vertex v1, Vertex v2){
+    return (v1.x - EPSILON < v2.x && v1.x + EPSILON > v2.x &&
+            v1.y - EPSILON < v2.y && v1.y + EPSILON > v2.y);
+}
+bool Graph::hVerticesEqual(float x1, float y1, float x2, float y2){
+    return (x1 - EPSILON < x2 && x1 + EPSILON > x2 &&
+            y1 - EPSILON < y2 && y1 + EPSILON > y2);
+}
+
 // crease start and finish, on a scale of 0 to 1
-void Graph::addVerticesWithEdge(float x1, float y1, float x2, float y2){
-    unsigned short v1 = v.size();
-    unsigned short v2 = v.size()+1;
-    Edge e1 = {v1, v2};
-    addVerticesWithoutEdge(x1, y1, x2, y2);
-    e.push_back(e1);
+void Graph::addEdgeWithVertices(float x1, float y1, float x2, float y2){
+    unsigned short vI1 = vertices.size();
+    unsigned short vI2 = vertices.size()+1;
+    Edge e1 = {vI1, vI2};
+
+    // add vertices
+    Vertex v1 = {x1, y1};
+    Vertex v2 = {x2, y2};
+    vertices.push_back(v1);
+    vertices.push_back(v2);
+    
+    // add edge
+    edges.push_back(e1);
 }
 
 bool Graph::mergeVertices(unsigned int vertexIndex1, unsigned int vertexIndex2){
@@ -129,7 +80,6 @@ bool Graph::mergeVertices(unsigned int vertexIndex1, unsigned int vertexIndex2){
     // retains the smaller index of the two
     unsigned int one, two;
     if(vertexIndex1 == vertexIndex2) {
-//        printf("GRAPH: mergeVertices(): indices are identical\n");
         return false;
     }
     if(vertexIndex1 < vertexIndex2) {one = vertexIndex1; two = vertexIndex2;}
@@ -137,27 +87,24 @@ bool Graph::mergeVertices(unsigned int vertexIndex1, unsigned int vertexIndex2){
 
     // replace all instances in EDGE array
     // and decrement all indices greater than vertexIndex2 (vertex array is about to lose vertexIndex2)
-    for(int i = 0; i < e.size(); i++){
-        if     (e[i].v1 == two) e[i].v1 = one;
-        else if(e[i].v1 > two)  e[i].v1--;
-        if     (e[i].v2 == two) e[i].v2 = one;
-        else if(e[i].v2 > two)  e[i].v2--;
+    for(int i = 0; i < edges.size(); i++){
+        if     (edges[i].v1 == two) edges[i].v1 = one;
+        else if(edges[i].v1 > two)  edges[i].v1--;
+        if     (edges[i].v2 == two) edges[i].v2 = one;
+        else if(edges[i].v2 > two)  edges[i].v2--;
     }
-    v.erase(v.begin()+two);
+    vertices.erase(vertices.begin()+two);
     return true;
 }
 
 void Graph::cleanup(){
-    float ELBOW = 0.001;
-    
-    int j, i = 0;
-    while(i < v.size()-1){
-        j = 0;
-        while(j < v.size()){
+    int i = 0;
+    while(i < vertices.size()-1){
+        int j = i+1;
+        while(j < vertices.size()){
             bool didRemove = false;
-            // quick and easy, use a square bounding box, do the points overlap?
-            if (v[i].x - ELBOW < v[j].x && v[i].x + ELBOW > v[j].x &&
-                v[i].y - ELBOW < v[j].y && v[i].y + ELBOW > v[j].y ){
+            // do the points overlap?
+            if ( hVerticesEqual(vertices[i], vertices[j]) ){
                 didRemove = mergeVertices(i, j);
             }
             // only iterate forward if we didn't remove an element
@@ -170,29 +117,44 @@ void Graph::cleanup(){
     }
 }
 
-
-void Graph::addVerticesWithoutEdge(float x1, float y1, float x2, float y2){
-    Vertex v1 = {x1, y1};
-    Vertex v2 = {x2, y2};
-    v.push_back(v1);
-    v.push_back(v2);
+bool Graph::edgeAdjacent(unsigned int edgeIndex1, unsigned int edgeIndex2){
+    return ( (edges[edgeIndex1].v1 == edges[edgeIndex2].v1) ||
+             (edges[edgeIndex1].v2 == edges[edgeIndex2].v2) ||
+             (edges[edgeIndex1].v1 == edges[edgeIndex2].v2) ||
+             (edges[edgeIndex1].v2 == edges[edgeIndex2].v1) );
 }
-
 
 bool Graph::isValid(){
     invalidEdgeCrossings();
 }
 
+vector<unsigned int> Graph::edgesIntersectingEdges(){
+    vector<unsigned int> invalidEdges;
+    for(int i = 0; i < edges.size() - 1; i++){
+        for(int j = i+1; j < edges.size(); j++){
+            if(!edgeAdjacent(i, j) && doIntersect(vertices[ edges[i].v1 ],
+                                                  vertices[ edges[i].v2 ],
+                                                  vertices[ edges[j].v1 ],
+                                                  vertices[ edges[j].v2 ])){
+                // true: edges i and j overlap
+                invalidEdges.push_back(i);
+                invalidEdges.push_back(j);
+            }
+        }
+    }
+    return invalidEdges;
+}
+
 bool Graph::invalidEdgeCrossings(){
     float SCALE = 100.0;
-    for(int i = 0; i < e.size(); i++){
-        for(int j = 0; j < e.size(); j++){
+    for(int i = 0; i < edges.size(); i++){
+        for(int j = 0; j < edges.size(); j++){
             if(i != j){
                 Vertex vertex[4];
-                vertex[0] = v[ e[i].v1 ];
-                vertex[1] = v[ e[i].v2 ];
-                vertex[2] = v[ e[j].v1 ];
-                vertex[3] = v[ e[j].v2 ];
+                vertex[0] = vertices[ edges[i].v1 ];
+                vertex[1] = vertices[ edges[i].v2 ];
+                vertex[2] = vertices[ edges[j].v1 ];
+                vertex[3] = vertices[ edges[j].v2 ];
                 for(int k = 0; k < 4; k++){
                     vertex[k].x *= SCALE;
                     vertex[k].y *= SCALE;
@@ -208,13 +170,13 @@ bool Graph::invalidEdgeCrossings(){
 
 bool Graph::edgeIsValid(unsigned int edgeIndex){
     float SCALE = 100.0;
-    for(int i = 0; i < e.size(); i++){
+    for(int i = 0; i < edges.size(); i++){
         if(edgeIndex != i){
             Vertex vertex[4];
-            vertex[0] = v[ e[edgeIndex].v1 ];
-            vertex[1] = v[ e[edgeIndex].v2 ];
-            vertex[2] = v[ e[i].v1 ];
-            vertex[3] = v[ e[i].v2 ];
+            vertex[0] = vertices[ edges[edgeIndex].v1 ];
+            vertex[1] = vertices[ edges[edgeIndex].v2 ];
+            vertex[2] = vertices[ edges[i].v1 ];
+            vertex[3] = vertices[ edges[i].v2 ];
             for(int j = 0; j < 4; j++){
                 vertex[j].x *= SCALE;
                 vertex[j].y *= SCALE;
@@ -230,34 +192,75 @@ bool Graph::edgeIsValid(unsigned int edgeIndex){
 
 
 void Graph::findAndReplaceInstancesEdge(int *newVertexIndexMapping){
-    for(int i = 0; i < e.size(); i++){
-        if(newVertexIndexMapping[ e[i].v1 ] != -1){
-            e[i].v1 = newVertexIndexMapping[ e[i].v1 ];
+    for(int i = 0; i < edges.size(); i++){
+        if(newVertexIndexMapping[ edges[i].v1 ] != -1){
+            edges[i].v1 = newVertexIndexMapping[ edges[i].v1 ];
         }
-        if(newVertexIndexMapping[ e[i].v2 ] != -1){
-            e[i].v2 = newVertexIndexMapping[ e[i].v2 ];
+        if(newVertexIndexMapping[ edges[i].v2 ] != -1){
+            edges[i].v2 = newVertexIndexMapping[ edges[i].v2 ];
         }
     }
 }
 
 void Graph::log(){
     printf("\nVertices:\n");
-    for(int i = 0; i < v.size(); i++)
-        printf(" %d: (%f, %f, %f)\n", i, v[i].x, v[i].y, v[i].z);
+    for(int i = 0; i < vertices.size(); i++)
+        printf(" %d: (%f, %f, %f)\n", i, vertices[i].x, vertices[i].y, vertices[i].z);
     printf("\nEdges:\n");
-    for(int i = 0; i < e.size(); i++)
-        printf(" %d: (%d -- %d)\n", i, e[i].v1, e[i].v2);
+    for(int i = 0; i < edges.size(); i++)
+        printf(" %d: (%d -- %d)\n", i, edges[i].v1, edges[i].v2);
+}
+
+
+bool Graph::vertexLiesOnEdge(unsigned int vIndex, Vertex *intersect){
+    Vertex v = vertices[vIndex];
+    return vertexLiesOnEdge(v, intersect);
+}
+
+bool Graph::vertexLiesOnEdge(Vertex v, Vertex *intersect){
+    // including a margin of error, bounding area around vertex
+    
+    // first check if point lies on end points
+    for(int i = 0; i < vertices.size(); i++){
+        if( hVerticesEqual(vertices[i], v) ){
+            intersect->x = vertices[i].x;
+            intersect->y = vertices[i].y;
+            intersect->z = vertices[i].z;
+            return true;
+        }
+    }
+    
+    for(int i = 0; i < edges.size(); i++){
+        Vertex a = vertices[ edges[i].v1 ];
+        Vertex b = vertices[ edges[i].v2 ];
+        float crossproduct = (v.y - a.y) * (b.x - a.x) - (v.x - a.x) * (b.y - a.y);
+        if(fabs(crossproduct) < EPSILON){
+            // cross product is essentially zero, point lies along the (infinite) line
+            // now check if it is between the two points
+            float dotproduct = (v.x - a.x) * (b.x - a.x) + (v.y - a.y) * (b.y - a.y);
+            // dot product must be between 0 and the squared length of the line segment
+            if(dotproduct > 0){
+                float lengthSquared = powf(b.x - a.x, 2) + powf(b.y - a.y, 2);
+                if(dotproduct < lengthSquared){
+                    //TODO: intersection
+//                    intersect =
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 vector<unsigned int> Graph::connectedVertexIndices(unsigned int vIndex){
     vector<unsigned int> indices;
     // iterate over all edges
-    for(int i = 0; i < e.size(); i++){
+    for(int i = 0; i < edges.size(); i++){
         // if we find our index, add the vertex on the other end of the edge
-        if(this->e[i].v1 == vIndex)
-            indices.push_back(this->e[i].v2);
-        if(this->e[i].v2 == vIndex)
-            indices.push_back(this->e[i].v1);
+        if(this->edges[i].v1 == vIndex)
+            indices.push_back(this->edges[i].v2);
+        if(this->edges[i].v2 == vIndex)
+            indices.push_back(this->edges[i].v1);
     }
     return indices;
 }
@@ -265,9 +268,9 @@ vector<unsigned int> Graph::connectedVertexIndices(unsigned int vIndex){
 vector<unsigned int> Graph::connectingEdgeIndices(unsigned int vIndex){
     vector<unsigned int> indices;
     // iterate over all edges
-    for(int i = 0; i < e.size(); i++){
+    for(int i = 0; i < edges.size(); i++){
         // if we find our vertex, add the edge
-        if(this->e[i].v1 == vIndex || this->e[i].v2 == vIndex)
+        if(this->edges[i].v1 == vIndex || this->edges[i].v2 == vIndex)
             indices.push_back(i);
     }
     return indices;
@@ -280,8 +283,8 @@ vector<unsigned int> Graph::connectingVertexIndicesSortedRadially(unsigned int v
     // so we can locally sort each clockwise or counter clockwise
     vector<float> sortedGlobalAngleValues;
     for(int i = 0; i < connectedVertices.size(); i++){
-        float angle = atan2(this->v[connectedVertices[i]].y - this->v[vIndex].y,
-                            this->v[connectedVertices[i]].x - this->v[vIndex].x);
+        float angle = atan2(this->vertices[connectedVertices[i]].y - this->vertices[vIndex].y,
+                            this->vertices[connectedVertices[i]].x - this->vertices[vIndex].x);
         globalAngleValues.push_back( angle );
         sortedGlobalAngleValues.push_back( angle );
     }
@@ -299,8 +302,8 @@ vector<float> Graph::connectingVertexInteriorAngles(unsigned int vIndex, vector<
     vector<float> anglesBetweenVertices;
     vector<float> anglesOfVertices;
     for(int i = 0; i < connectedVertexIndicesSorted.size(); i++){
-        float angle = atan2(this->v[connectedVertexIndicesSorted[i]].y - this->v[vIndex].y,
-                            this->v[connectedVertexIndicesSorted[i]].x - this->v[vIndex].x);
+        float angle = atan2(this->vertices[connectedVertexIndicesSorted[i]].y - this->vertices[vIndex].y,
+                            this->vertices[connectedVertexIndicesSorted[i]].x - this->vertices[vIndex].x);
         anglesOfVertices.push_back(angle);
     }
     for(int i = 0; i < anglesOfVertices.size(); i++){
@@ -314,9 +317,71 @@ vector<float> Graph::connectingVertexInteriorAngles(unsigned int vIndex, vector<
 }
 
 void Graph::rotateEdge(int index, int indexOrigin, float angle){
-    float distance = sqrt(powf( this->v[indexOrigin].y - this->v[index].y ,2)
-                          +powf( this->v[indexOrigin].x - this->v[index].x ,2));
-    float currentAngle = atan2(this->v[index].y, this->v[index].x);
-    this->v[index].x = distance*cosf(currentAngle + angle);
-    this->v[index].y = distance*sinf(currentAngle + angle);
+    float distance = sqrt(powf( this->vertices[indexOrigin].y - this->vertices[index].y ,2)
+                          +powf( this->vertices[indexOrigin].x - this->vertices[index].x ,2));
+    float currentAngle = atan2(this->vertices[index].y, this->vertices[index].x);
+    this->vertices[index].x = distance*cosf(currentAngle + angle);
+    this->vertices[index].y = distance*sinf(currentAngle + angle);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//
+//                            2D ALGORITHMS
+//
+
+// if points are all collinear
+// checks if point q lies on line segment 'ab'
+bool onSegment(Vertex a, Vertex point, Vertex b){
+    if (point.x <= max(a.x, b.x) && point.x >= min(a.x, b.x) &&
+        point.y <= max(a.y, b.y) && point.y >= min(a.y, b.y))
+        return true;
+    return false;
+}
+
+
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are collinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+int orientation(Vertex p, Vertex q, Vertex r){
+    // See http://www.geeksforgeeks.org/orientation-3-ordered-points/
+    // for details of below formula.
+    float val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    if (fabs(val) <= EPSILON) return 0;  // collinear
+    return (val > 0)? 1: 2; // clock or counterclock wise
+}
+
+// The main function that returns true if line segment 'p1q1'
+// and 'p2q2' intersect.
+bool doIntersect(Vertex p1, Vertex q1, Vertex p2, Vertex q2){
+    // Find the four orientations needed for general and
+    // special cases
+    int o1 = orientation(p1, q1, p2);
+    int o2 = orientation(p1, q1, q2);
+    int o3 = orientation(p2, q2, p1);
+    int o4 = orientation(p2, q2, q1);
+    
+    // General case
+    if (o1 != o2 && o3 != o4)
+        return true;
+    
+    // Special Cases
+    // p1, q1 and p2 are colinear and p2 lies on segment p1q1
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+    
+    // p1, q1 and p2 are colinear and q2 lies on segment p1q1
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+    
+    // p2, q2 and p1 are colinear and p1 lies on segment p2q2
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+    
+    // p2, q2 and q1 are colinear and q1 lies on segment p2q2
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+    
+    return false; // Doesn't fall in any of the above cases
 }
--- graph.h
+++ graph.h
@@ -1,11 +1,9 @@
 #include "ofMain.h"
 
-//using namespace std;
 
 typedef struct Vertex{
     double x, y, z;
 } Vertex;
-
 typedef struct Edge {
     unsigned short v1, v2;
 } Edge;
@@ -16,27 +14,46 @@ int orientation(Vertex p, Vertex q, Vertex r);
 bool doIntersect(Vertex a1, Vertex a2, Vertex b1, Vertex b2);
 
 
-class Graph : public ofBaseApp{
-public:
-    vector <Edge> e;
-    vector <Vertex> v;
+// this graph represents an origami crease pattern
+//    with creases (edges) defined by their endpoints (vertices)
+//    for now, coordinate space is a square (0,0) (0,1) (1,1) (1,0)
 
+class Graph : public ofBaseApp{
+    
+//private:
+public:
+    // creases are lines (edges) with endpoints (v1, v2) which are
+    //   indices pointing to vertices in the vertices array
+    vector <Edge> edges;
+    vector <Vertex> vertices;
+    
+public:
+  
+    // constructors
     Graph( int initial );
     
-    
-    // joins points lying on top of each other
+    // removes all content (edges and vertices)
+    void clear();
+
+    // remove all duplicate vertices
     void cleanup();
     
-    void addVerticesWithEdge(float x1, float y1, float x2, float y2);
-    void addVerticesWithoutEdge(float x1, float y1, float x2, float y2);
+    // tidies up the inside of the arrays
+    // organizes the vertices by position in space, edges by vertices
+//    void defragment();
     
-    // crease start and finish, on a scale of 0 to 1
-    void crease(float x1, float y1, float x2, float y2);
+    // add a crease line start (x,y) to finish (x,y). scale from 0 to 1
+    void addEdgeWithVertices(float x1, float y1, float x2, float y2);
     
-    // get back all the vertices that are connected to the input vertex by edges
+    bool edgeAdjacent(unsigned int edgeIndex1, unsigned int edgeIndex2);
+    
+    // needs to be a set, has duplicates
+    vector<unsigned int> edgesIntersectingEdges();
+
+    // find all the vertices that are connected argument vertex by edges
     vector<unsigned int> connectedVertexIndices(unsigned int vIndex);
     
-    // get back all the edges which join to the input vertex
+    // find all the edges which join to the input vertex
     vector<unsigned int> connectingEdgeIndices(unsigned int vIndex);
     
     // same as above, sorted radially by neighbor
@@ -50,13 +67,32 @@ public:
     
     void log();
     
+    // getters
+    unsigned int numVertices();
+    unsigned int numEdges();
+    
     bool isValid();
     
     bool invalidEdgeCrossings();
     bool edgeIsValid(unsigned int edgeIndex);
     
+    
+    // technically includes "vertex lies on a vertex"
+    //   returns the point of intersection (as an argument)
+    bool vertexLiesOnEdge(unsigned int vIndex, Vertex *intersect);
+    bool vertexLiesOnEdge(Vertex v, Vertex *intersect);
+    
+    bool edgeCrossesEdge(unsigned int index1, unsigned int index2);
+    bool edgeCrossesEdge(Vertex a1, Vertex a2, Vertex b1, Vertex b2);
+    
 private:
+    
+    bool hVerticesEqual(float x1, float y1, float x2, float y2);
+    bool hVerticesEqual(Vertex v1, Vertex v2);
+
+    
     bool mergeVertices(unsigned int vIndex1, unsigned int vIndex2);
     void findAndReplaceInstancesEdge(int *newVertexIndexMapping);
+
 };
 
