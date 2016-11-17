e8ffdbf8e135be49274ce886291926bd4134ca1c
robbykraft <robbykraft@gmail.com>
2016-11-17 02:45:41 -0500

    multiple sketches, namespace, tests on one page

---
+++ javascript/renderNS.js
@@ -0,0 +1,52 @@
+
+var drawGraphLines = function(p, planarGraph){
+	var point_array = [];
+	for(var i = 0; i < planarGraph.nodes.length; i++){
+		var v = planarGraph.nodes[i];
+		point_array[i*3+0] = v.x;
+		point_array[i*3+1] = v.y;
+		point_array[i*3+2] = v.z;
+	}
+	var line_array = [];
+	for(var i = 0; i < planarGraph.edges.length; i++){
+		line_array[i*2+0] = planarGraph.edges[i].a;
+		line_array[i*2+1] = planarGraph.edges[i].b;
+	}
+	// DRAW THINGS
+	for(var i = 0; i < line_array.length*0.5; i++){
+		p.line(point_array[ line_array[i*2+0]*3+0 ], point_array[ line_array[i*2+0]*3+1 ], 
+		     point_array[ line_array[i*2+1]*3+0 ], point_array[ line_array[i*2+1]*3+1 ] );
+	}
+}
+
+var drawCoordinateFrame = function(p){
+	// draws a box around the (0,0) to (1,1) square space
+	var corners = [0.0, 0.0, 0.0,
+	               0.0, 1.0, 0.0,
+	               1.0, 1.0, 0.0,
+	               1.0, 0.0, 0.0 ];
+	var cornerLines = [0, 1, 1, 2, 2, 3, 3, 0];
+	for(var a = 0; a < 4; a++){
+		var b = (a+1)%4;
+		p.line(corners[a*3+0], corners[a*3+1],
+		     corners[b*3+0], corners[b*3+1]);
+	}
+	// setColor(0.2, 0.2, 0.2);
+	// DRAW LINES
+}
+
+var drawGraphPoints = function(p, planarGraph){
+	var point_array = [];
+	var numPoints = 0;
+	for(var i = 0; i < planarGraph.nodes.length; i++){
+		var v = planarGraph.nodes[i];
+		point_array[i*3+0] = v.x;
+		point_array[i*3+1] = v.y;
+		point_array[i*3+2] = v.z;
+		numPoints += 1;
+	}
+	// DRAW THINGS
+	for(var i = 0; i < numPoints; i++){
+		p.ellipse(point_array[i*3+0], point_array[i*3+1], .01, .01);
+	}
+}
