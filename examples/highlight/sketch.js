b91b3eb2379b4010897ae1ea70dbd5e3c7d8bc8b
Robby Kraft <robbykraft@gmail.com>
2018-09-14 19:04:12 -0400

    svg working well, one example ported over

--- examples/highlight/sketch.js
+++ examples/highlight/sketch.js
@@ -1,33 +1,20 @@
 // example
 // mouse hover over nodes, faces, edges, sectors to highlight them
 var cp = new CreasePattern().frogBase();
-var origami = new OrigamiPaper("canvas-cp", cp);
-
-// by default only edges are shown
-origami.show.nodes = true;
-origami.show.faces = true;
-origami.show.sectors = true;
-
-// set all fill colors transparent, only turn on each one upon hover
-origami.style.node.fillColor = {alpha:0.0};
-origami.style.face.fillColor = {alpha:0.0};
-origami.style.sector.fillColors = [{alpha:0.0}, {alpha:0.0}];
-
-// this is required if new components are shown. they didn't yet exist on the canvas
-// (todo: this is hard to remember and shouldn't be required. need a work-around)
-origami.draw();
+var origami = new OrigamiPaper(cp);
 
 origami.onMouseMove = function(event){
-	// update() returns all crease lines back to their original color
+	// update returns all components back to their original color
 	origami.update();
+	// now we set the colors of the components we want
 
-	// get the nearest parts of the crease pattern to the mouse point
-	var nearest = cp.nearest(event.point);
+	// get all the nearest components to the cursor
+	var nearest = origami.cp.nearest(event);
 
-	// get() will return the paperjs object (line, polygon) that reflects the data model object
-	// this paperjs object is what we style
-	origami.get(nearest.node).fillColor = this.styles.byrne.darkBlue;
-	origami.get(nearest.edge).strokeColor = this.styles.byrne.yellow;
-	origami.get(nearest.face).fillColor = this.styles.byrne.red;
-	origami.get(nearest.sector).fillColor = this.styles.byrne.blue;
+	// get() gives us the SVG element that corresponds to the crease pattern data component
+	origami.get(nearest.node).setAttribute('class', 'fill-dark-blue');
+	origami.get(nearest.edge).setAttribute('class', 'stroke-yellow');
+	origami.get(nearest.face).setAttribute('class', 'fill-red');
+	origami.get(nearest.sector).setAttribute('class', 'fill-blue');
+
 }
--- examples/svg/sketch.js
+++ examples/svg/sketch.js
@@ -4,11 +4,11 @@
 // var origami = new OrigamiPaper(cp);
 
 var origami = new OrigamiPaper();
-origami.load("../../files/svg/crane.svg");
-// origami.load("all-shapes.svg");
+origami.style.node.radius = 0.005;
+
+// origami.load("../../files/svg/crane.svg");
+origami.load("all-shapes.svg");
 // origami.load("rects.svg");
-
-
 
 // var svg = document.getElementById('mysvg');
 // var NS = svg.getAttribute('xmlns');
@@ -19,7 +19,7 @@ origami.onMouseMove = function(point){
 	if (edge != undefined){
 		var svgEdge = document.getElementById("edge-" + edge.index);
 		if(svgEdge != undefined){
-			svgEdge.setAttributeNS(null, 'class', 'highlighted');
+			svgEdge.setAttributeNS(null, 'class', 'stroke-yellow');
 		}
 	}
 }
@@ -27,7 +27,6 @@ origami.onMouseMove = function(point){
 origami.onMouseDown = function(point){
 	console.log("on mouse down ", point);
 }
-
 
 origami.onFrame = function(event){
 	console.log(this.mouse);
